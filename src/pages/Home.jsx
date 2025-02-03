5c8871a96f5983efcc9470f94949e294db38e72c
afterglow <cotmdals0@naver.com>
2025-02-03 18:46:24 +0900

    slider 수정 요망

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 import './Home.scss';
+
 import {Mobile, PC} from './Mediaquery.jsx';
 import Navbar from "./Navbar.jsx";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
 function Home(){
+
+    const FAQData = [
+        { question: "동아리 가입 방법은?", answer: "동아리 지원하기를 통해 지원하시면 연락드리겠습니다." },
+        { question: "동아리 회비는?", answer: "동아리 회비는 없습니다.\n다만, 대회 참여, 자재 구입 등 불가피한 경우 활동에 참여한 인원들과 금액을 분담할 수 있습니다." },
+        { question: "경험이 없어도 프로젝트(대회) 참여가 가능한가요?", answer: "그럼요! stdio.h의 모든 활동은 학년, 경험 등에 제한을 두지 않습니다.\n하고싶다는 의지 하나면 충분합니다!" },
+        { question: "지원 자격은?", answer: "스마트ICT융합공학과 또는 전자IT미디어공학과(미디어트랙) 학생(학년무관)이면 누구나 가능합니다!" },
+        { question: "활동 소식은 어디서?", answer: "동아리의 활동 소식은 이 페이지 Achive 탭이나 네이버 카페를 참조해주세요." }
+    ];
+    const [openFAQ, setOpenFAQ] = useState(Array(FAQData.length).fill(true));
+    const toggleFAQ = (index) =>{
+        setOpenFAQ(prev => {
+            const newFAQ = [...prev];
+            newFAQ[index] = !newFAQ[index];
+            return newFAQ;
+        });
+    };
+
     return(
         <div className="Home">
             <PC>
@@ -34,6 +59,82 @@ function Home(){
                                     </div>
                             </div>
                         </div>
+                    </div>
+
+                    <div className="Mob-FAQ">
+                        <div className="Mob-FAQ-inline">
+                            <div className="FAQ-title">
+                                <div className="FAQ-title-head">여러분을 위한 Q&A</div>
+                                <div className="FAQ-title-sub">여러분이 동아리에 지원하시기 전 자주묻는 질문을 모아봤습니다</div>
+                            </div>
+
+                            <div className="FAQ-list">
+                                {FAQData.map((faq, index) => (
+                                    <div className="FAQ-item" key={index}>
+                                        <div className="Q" onClick={() => toggleFAQ(index)}>
+                                            <div className="Q-text">{faq.question}</div>
+                                            <img src="./top.svg" className={`Q-arrow ${openFAQ[index] ? '' : 'rotate'}`}/>
+                                        </div>
+                                        {openFAQ[index] && (
+                                            <div className="A">
+                                                <div className="A-text">{faq.answer}</div>
+                                            </div>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Achieve">
+                        <div className="Achieve-Head">stdio.h의 기록</div>
+                        <div className="Achieve-Sub">동아리원들이 함께 협동해서 이룬 성과들 입니다</div>
+
+                        <Swiper
+                            // install Swiper modules
+                            modules={[Navigation, Pagination]}
+                            spaceBetween={50}
+                            slidesPerView={2}
+                            navigation = {{
+                                nextEl: '.custom-next',
+                                prevEl: '.custom-prev',
+                            }}
+                            pagination={{ clickable: true }}
+                            onSwiper={(swiper) => console.log(swiper)}
+                            onSlideChange={() => console.log('slide change')}
+                            >
+                            <SwiperSlide className="tile">
+                                <div className="tile-head">2023 RE-UP Cycle Thon
+                                (GREEN TECH MAKER)</div>
+                                <div className="tile-sub">최우수상 수상</div>
+                            </SwiperSlide>
+
+                            <SwiperSlide className="tile">
+                                <div className="tile-head">2023 RE-UP Cycle Thon
+                                (GREEN TECH MAKER)</div>
+                                <div className="tile-sub">최우수상 수상</div>
+                            </SwiperSlide>
+
+                            <SwiperSlide className="tile">
+                                <div className="tile-head">2023 RE-UP Cycle Thon
+                                    (GREEN TECH MAKER)</div>
+                                <div className="tile-sub">최우수상 수상</div>
+                            </SwiperSlide>
+
+                            <SwiperSlide className="tile">
+                                <div className="tile-head">2023 RE-UP Cycle Thon
+                                (GREEN TECH MAKER)</div>
+                                <div className="tile-sub">최우수상 수상</div>
+                            </SwiperSlide>
+                            <div className="button">
+                                <div className="custom-prev">
+                                    <img src="./arrow-back-fill.svg" alt="Previous" />
+                                </div>
+                                <div className="custom-next">
+                                    <img src="./arrow-right-fill.svg" alt="Next" />
+                                </div>
+                            </div>
+                        </Swiper>
                     </div>
                 </div>
             </Mobile>
